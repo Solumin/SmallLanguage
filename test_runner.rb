2d27007ec59560fa43276c0b3e1b3952e8ab3859
Solumin <magesolumin@gmail.com>
2016-05-14 13:05:07 -0400

    Added dumb script for running examples

---
+++ test_runner.rb
@@ -0,0 +1,16 @@
+# Test files are in the 'examples/' directory
+tests = Dir['examples/*']
+# The executable:
+parser = "./small_parser.exe"
+# Failure states
+failure = { 2 => "Parsing failed" }
+
+tests.sort.each do |test|
+    output = `#{parser} #{test}`
+    if failure[$?.exitstatus]
+        puts "FAIL #{test}"
+        puts "\t#{output.lines[0]}"
+    else
+        puts "PASS #{test}"
+    end
+end
